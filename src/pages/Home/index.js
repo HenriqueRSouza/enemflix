808a977fa59ac40950bd5b2f5abed2c8663077bc
HenriqueRSouza <henriqueikesouza@gmail.com>
2020-08-03 10:16:32 -0300

    Three commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import Menu from '../../components/Menu/index.js';
+import dadosIniciais from '../../data/dados_iniciais.json'
+import BanerMain from '../../components/BannerMain/index.js'
+import Carousel from '../../components/Carousel/index.js'
 import Footer from '../../components/Footer/index'
+import BannerMain from '../../components/BannerMain/index.js';
 
 
 
 function Home() {
   return (
-    <div>
-      <Menu  /> 
+    <div style={{background: "#141414"}}>
+      <Menu  />
+      <BannerMain 
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end"}
+      />  
+      <Carousel 
+        category={dadosIniciais.categorias[1]}
+        />
+      <Carousel 
+        category={dadosIniciais.categorias[2]}
+        />
+      <Carousel 
+        category={dadosIniciais.categorias[3]}
+        />
+      <Carousel 
+        category={dadosIniciais.categorias[4]}
+        />
+      <Carousel 
+        category={dadosIniciais.categorias[5]}
+        />
       <Footer />
     </div>
   );
