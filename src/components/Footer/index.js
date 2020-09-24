4ec07a1a48687ea793c8806ae1c0258e79f26e35
Henrique Ribeiro <henriqueribeiro@pop-os.localdomain>
2020-09-24 09:29:46 -0300

    Final Commit School

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -14,6 +14,12 @@ function Footer() {
         <a href="https://www.google.com/search?biw=1366&bih=696&sxsrf=ALeKk03Z-DOCbCjrg6_3n52N6qjQM-7zGA%3A1596220527141&ei=b2QkX8KkCK-x5OUP4JiNwAg&q=mortes+por+covid+em+sp&oq=Mortes+por&gs_lcp=CgZwc3ktYWIQAxgBMgUIABCxAzIFCAAQsQMyBAgAEEMyAggAMgUIABCxAzICCAAyBQgAELEDMgIIADICCAAyBQgAELEDOgQIIxAnOgQILhAnOggILhCxAxCDAToICAAQsQMQgwE6BAguEEM6BwgjEOoCECc6CQgjEOoCECcQEzoHCAAQsQMQQzoKCAAQsQMQgwEQQ1CUNFicTmCqY2gCcAB4AIABeYgBuwmSAQM2LjaYAQCgAQGqAQdnd3Mtd2l6sAEKwAEB&sclient=psy-ab">
           Quarentena
         </a>
+        <br></br>
+        <br></br>
+        <a href="https://youtu.be/2tn2SnaMRx0">Onde tudo começou !!</a>
+        <br></br>
+        <br></br>
+        <a>Site criado com intenção de ajudar os vestibulandos a estudar !</a>
       </p>
     </FooterBase>
   );
